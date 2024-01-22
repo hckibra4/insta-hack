4e17154cf777eccad4ee1d255d44813e2730bd86
Kara kuş <127147952+hckibra4@users.noreply.github.com>
2024-01-22 23:58:42 +0300

    Add files via upload

---
+++ insta-pasaword-hack.py
@@ -0,0 +1,25 @@
+/* -----------------------------------------------
+/* Author : ibrahim21 -ibrahim21.In
+
+/* GitHub : https://github.com/ibrhim21/SocialBox.git
+/* ----------------------------------------------- */
+  from random import choice,choice,choices
+       numbers=(1,2,3,4,5,6,7,8,9,0,"İbrahim","adaballi")
+    password=str(input("Kırılacak şifreyi gir"))
+       passwords""
+       
+       step=0
+       
+while True:
+
+print("şifreniz denetleniyor...",passwords)
+  if password==passwords:break
+    else: 
+   
+   passwords:""
+   
+   for i in range(4):passwords+=str(choice(numbers))
+   step+=1
+   
+   print("şifreniz başarıyla kırılmıştır",password,"adımda kırılmış",step)
+   
